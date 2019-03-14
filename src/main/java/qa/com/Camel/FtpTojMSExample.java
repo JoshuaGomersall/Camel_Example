e9cc89d6391b7cc7166be4f2615cb30bc105ab49
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-03-14 16:30:33 +0000

    2Nd Commit

---
+++ src/main/java/qa/com/Camel/FtpTojMSExample.java
@@ -0,0 +1,34 @@
+package qa.com.Camel;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+public class FtpTojMSExample {
+
+	public static void main(String args[]) throws Exception {
+		while (true) {
+			CamelContext context = new DefaultCamelContext();
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+			context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+			context.addRoutes(new RouteBuilder() {
+				@Override
+				public void configure() {
+					from("ftp://test.rebex.net?username=demo&password=password").to("activemq:queue:HumanFighters");
+
+					from("ftp://test.rebex.net/pub/example?username=demo&password=password").to("activemq:queue:HumanFighters");
+				}
+			});
+			context.start();
+			Thread.sleep(10000);
+			context.stop();
+		}
+	}
+
+}
